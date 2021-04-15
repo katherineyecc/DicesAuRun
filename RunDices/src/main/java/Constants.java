d3c196eaf65bddf8b30929ee4aa055052926b6ab
Katherine <41030232+katherineyecc@users.noreply.github.com>
2021-04-15 21:06:03 +0800

    Add files via upload

---
+++ RunDices/src/main/java/Constants.java
@@ -0,0 +1,33 @@
+
+public class Constants {
+	public static final String jeneticsDicesPath = "/home/ubuntu/dices-app/dices_anonym";
+	public static final String jeneticsDicesOarPath = "/home/ubuntu/dices-app/dices_anonym/target/dices-app-1.0-SNAPSHOT.oar";
+	public static final String jeneticsDicesName = "abc.def.dices";
+	public static final String EcjDicesPath = "/home/ubuntu/dices-app-ECJ";
+	public static final String EcjDicesOarPath = "/home/ubuntu/dices-app-ECJ/target/dices-app-2.0-SNAPSHOT.oar";
+	public static final String EcjDicesName = "abc.def.dices.2";
+	public static final String onosPath = "/home/ubuntu/onos";
+	
+	public static final String wgetOnos = "sudo wget -c https://repo1.maven.org/maven2/org/onosproject/onos-releases/1.15.0/onos-1.15.0.tar.gz";
+    public static final String gitEcjDices = "git clone https://github.com/JiaLi123456/dices-app-ECJ.git";
+    public static final String gitJeneticsDices = "git clone https://github.com/JiaLi123456/dices-app.git";
+    public static final String unzipOnos = "sudo tar -zxf onos-1.15.0.tar.gz";
+    public static final String cpOnos = "sudo mv onos-1.15.0 onos";
+    public static final String rmOnosTar = "sudo rm -rf onos-1.15.0.tar.gz";
+    
+    public static final String onosLoginPwd = "rocks";
+    public static final String installJava11 = "sudo apt-get install openjdk-11-jdk openjdk-11-jre -y";
+    public static final String installJava8 = "sudo apt-get install openjdk-8-jdk openjdk-8-jre -y";
+    public static final String update = "sudo apt-get update";
+    public static final String upgrade = "sudo apt-get upgrade -y";
+    public static final String installgit = "sudo apt-get install git-all -y";
+    public static final String installzip = "sudo apt-get install zip -y";
+    public static final String installunzip = "sudo apt-get install unzip -y";
+    public static final String installcurl = "sudo apt-get install curl -y";
+    public static final String installpy2 = "sudo apt-get install python -y && sudo apt-get install python-pip python-dev python-setuptools -y";
+    public static final String installpy3 = "sudo apt-get install python3 -y && sudo apt-get install python3-pip python3-dev python3-setuptools -y";
+    public static final String installpip = "pip3 install --upgrade pip";
+    public static final String installselenium = "pip3 install selenium";
+    public static final String installrequests= "pip install requessudo adduser --disabled-password sdnts";
+    public static final String installmaven = "sudo apt-get install maven -y";
+}
